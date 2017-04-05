8ae66f70489c3e4bf315cbf1847e39b94315f945
Alexey Kuznetsov <alexey.kuznetsov@dailydeal.de>
2017-04-05 11:34:23 +0200

    add native metrics

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   "summary": "meteor new relic integration",
   "name": "alex509:newrelic",
-  "version": "1.0.0",
+  "version": "1.0.1",
   "author": "Alexey Kuznetsov (https://github.com/Alex509)",
   "homepage": "https://github.com/Alex509/meteor-newrelic",
   "git": "https://github.com/Alex509/meteor-newrelic.git"
 });
 
-Npm.depends({ "newrelic": "1.38.2" });
+Npm.depends({ "newrelic": "1.38.2", "@newrelic/native-metrics": "2.1.1" });
 
 Package.on_use(function(api) {
   api.versionsFrom("METEOR@1.3.1");
